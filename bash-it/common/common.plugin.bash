ba52440707c5303cb0fe5fe6c51b50ba867a7818
zanmato <ruoxi.sun@transwarp.io>
2018-03-14 14:12:02 +0800

    Finish bash common and goron.

---
+++ bash-it/common/common.plugin.bash
@@ -0,0 +1,98 @@
+cite about-plugin
+about-plugin 'common plugin'
+
+function fp ()
+{
+    about 'display full path of a file'
+    param '1: file name'
+    group 'mine'
+    if [ "${1::1}" == "/" ]; then
+      echo $1
+    else
+      echo $1 | sed "s:^:`pwd`/:"
+    fi
+}
+
+function clipcpy ()
+{
+    about 'copy stdin to clipboard'
+    group 'mine'
+    if [ $(uname) = "Darwin" ]; then
+        tr -d "\n" | pbcopy
+    elif [ $(uname) = "Linux" ]; then
+        tr -d "\n" | xsel -b
+    fi
+}
+
+function clippst ()
+{
+    about 'paste clipboard to stdout'
+    group 'mine'
+    if [ $(uname) = "Darwin" ]; then
+        pbpaste
+    elif [ $(uname) = "Linux" ]; then
+        xsel -b -o
+    fi
+}
+
+function selcpy ()
+{
+    about 'copy stdin to select'
+    group 'mine'
+    if [ $(uname) = "Darwin" ]; then
+        tr -d "\n" | pbcopy
+    elif [ $(uname) = "Linux" ]; then
+        tr -d "\n" | xsel
+    fi
+}
+
+function selpst ()
+{
+    about 'paste clipboard to stdout'
+    group 'mine'
+    if [ $(uname) = "Darwin" ]; then
+        pbpaste
+    elif [ $(uname) = "Linux" ]; then
+        xsel -o
+    fi
+}
+
+function cfp ()
+{
+    about 'copy full path of a file to clipboard'
+    param '1: file name'
+    group 'mine'
+    fp $* | clipcpy
+    echo "`clippst` copied to clipboard."
+}
+
+function sfp ()
+{
+    about 'copy full path of a file to select'
+    param '1: file name'
+    group 'mine'
+    fp $* | selcpy
+    echo "`selpst` copied to select"
+}
+
+function pk ()
+{
+    about 'kill the processes with specified name pattern'
+    param '1: process name pattern'
+    group 'mine'
+    for arg in $@
+    do
+        ps -u $USER | grep $arg | awk '{print $1}' | xargs kill -9
+    done
+}
+
+function jpk ()
+{
+    about 'kill the java processes with specified name pattern'
+    param '1: java process name pattern'
+    group 'mine'
+    for arg in $@
+    do
+        jps | grep $arg | awk '{print $1}' | xargs kill -9
+    done
+}
---
+++ bash-it/goron/goron.plugin.bash
@@ -0,0 +1,74 @@
+cite about-plugin
+about-plugin 'goron tools'
+
+function sce ()
+{
+    about 'copy full path of e.sql to select'
+    group 'mine'
+    sfp $DEVROOT/inceptor_idea/sql/e.sql
+}
+
+function ccf ()
+{
+    about 'copy full path of a file to clipboard and paste to f.sql'
+    param '1: file name'
+    group 'mine'
+    cfp $*
+    echo "source `clippst`" > $DEVROOT/inceptor_idea/sql/f.sql
+    echo "`clippst` sourced to $DEVROOT/inceptor_idea/sql/f.sql"
+}
+
+function scf ()
+{
+    about 'copy full path of a file to select and paste to f.sql'
+    param '1: file name'
+    group 'mine'
+    sfp $*
+    echo "source `selpst`" > $DEVROOT/inceptor_idea/sql/f.sql
+    echo "`selpst` sourced to $DEVROOT/inceptor_idea/sql/f.sql"
+}
+
+function warp ()
+{
+    about 'switch the working warp id'
+    param '1: warp number (4 digits)'
+    group 'mine'
+    export HIVE_BR=$SVN_ROOT/sbranches/ruoxi/warp-$1/hive-0.12.0-transwarp
+    export NGMR_BR=$SVN_ROOT/sbranches/ruoxi/warp-$1/ngmr-1.7-transwarp
+    export INCEPTOR_BR=$SVN_ROOT/sbranches/ruoxi/warp-$1/inceptor_idea
+}
+
+function rlbr ()
+{
+    about 'switch the working release branch'
+    param '1: release branch version (x.x)'
+    group 'mine'
+    export HIVE_BR=$SVN_ROOT/release_branches/transwarp-$1/hadoop/sources/hive-0.12.0-transwarp
+    export NGMR_BR=$SVN_ROOT/release_branches/transwarp-$1/hadoop/sources/ngmr-1.7-transwarp
+    export INCEPTOR_BR=$SVN_ROOT/release_branches/transwarp-$1/hadoop/sources/inceptor_idea
+}
+
+function devroot ()
+{
+    about 'switch the working devroot'
+    param '1: devroot path'
+    group 'mine'
+    rm $DEVROOT
+    ln -sf $1 $DEVROOT
+}
+
+function dsnst ()
+{
+    about 'De-snapshot the project version'
+    group 'mine'
+    sed -i "s/-SNAPSHOT//g" `find . -not -type d`
+}
+
+function bpvs ()
+{
+    about 'Bump the project version'
+    param '1: base version'
+    param '2: target version'
+    group 'mine'
+    sed -i "s/$1/$2/g" `find . -not -type d -and \( -name "*.java" -or -name "*.xml" -or -name "*.MF" -or -name "*.properties" -or -name "*.product" -or -name "*.sh" -or -name "*.md" \)`
+}

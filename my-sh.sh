8db6c2de349985cae5b8514e0fcd5ef610b48945
zanmato1984 <zanmato1984@gmail.com>
2018-04-02 14:17:19 +0800

    Fix tmux copy mode shortcuts.

--- my-sh.sh
+++ my-sh.sh
@@ -1,16 +1,11 @@
 if [ $(uname) = "Darwin" ]; then
-  export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home
-  export JAVA_VERSION=7
-  export MAVEN_HOME=/opt/apache-maven-3.3.3
+  export JAVA_HOME=`/usr/libexec/java_home`
   export GOROOT=/usr/local/opt/go/libexec
   export GODEBUG=netdns=go
 elif [ $(uname) = "Linux" ]; then
   export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-  export JAVA_VERSION=7
-  export MAVEN_HOME=/opt/apache-maven-3.3.3
   export GOROOT=/usr/local/go
 fi
-export SCALA_HOME=/usr/lib/scala-2.11.1
+# export SCALA_HOME=/usr/lib/scala-2.11.1
 export GOPATH=$HOME/dev/go
-export GOROOT=/usr/local/go
 export PATH=$MY_SH_HOME/scripts/common:$MY_SH_HOME/scripts/$MY_SH_DOMAIN:$JAVA_HOME:$JAVA_HOME/bin:$SCALA_HOME/bin:$MAVEN_HOME/bin:$(go env GOPATH)/bin:$GOROOT/bin:$PATH

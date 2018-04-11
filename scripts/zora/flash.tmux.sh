346b8eed49156aa7e743317ff9fe830d573d69a6
zanmato1984 <zanmato1984@gmail.com>
2018-04-11 13:24:40 +0800

    Add flash tmux script.

---
+++ scripts/zora/flash.tmux.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+tmux new-session -d -s flash -n ch
+tmux send-keys "cd $HOME/dev/theflash/ch-connector" "C-m"
+
+tmux new-window -n ch-run
+tmux send-keys "cd $HOME/dev/theflash/benchmark" "C-m"
+
+tmux new-window -n spark
+tmux send-keys "cd $HOME/dev/theflash/spark-connector" "C-m"
+
+tmux new-window -n spark-debug
+tmux send-keys "cd $HOME/dev/theflash/benchmark/sql-spark" "C-m"
+tmux split-window
+tmux send-keys "cd $HOME/dev/theflash/benchmark" "C-m"
+
+tmux select-window -t 1
+tmux a -t flash

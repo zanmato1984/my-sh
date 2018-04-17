4f4a831c7463b4fafca725c4b5bb75b86b52d6e1
zanmato1984 <zanmato1984@gmail.com>
2018-04-17 21:08:04 +0800

    Split ch window for flash tmux.

--- scripts/zora/flash.tmux.sh
+++ scripts/zora/flash.tmux.sh
@@ -5,6 +5,8 @@ tmux send-keys "cd $HOME/dev/theflash/ch-connector" "C-m"
 
 tmux new-window -n ch-run
 tmux send-keys "cd $HOME/dev/theflash/benchmark" "C-m"
+tmux split-window
+tmux send-keys "cd $HOME/dev/theflash/benchmark" "C-m"
 
 tmux new-window -n spark
 tmux send-keys "cd $HOME/dev/theflash/spark-connector" "C-m"

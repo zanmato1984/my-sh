4bba2e7be764cd1301f7409d8566cb296451483c
zanmato1984 <zanmato1984@gmail.com>
2018-05-04 14:31:07 +0800

    Add select-pane for ch window.

--- scripts/zora/flash.tmux.sh
+++ scripts/zora/flash.tmux.sh
@@ -7,6 +7,7 @@ tmux new-window -n ch-run
 tmux send-keys "cd $HOME/dev/theflash/benchmark" "C-m"
 tmux split-window
 tmux send-keys "cd $HOME/dev/theflash/benchmark" "C-m"
+tmux select-pane -t 1
 
 tmux new-window -n spark
 tmux send-keys "cd $HOME/dev/theflash/spark-connector" "C-m"

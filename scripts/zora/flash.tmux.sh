d25234e2b8bb7b5db85eb82476c85ad0f07f7881
zanmato1984 <zanmato1984@gmail.com>
2018-06-15 02:19:38 +0800

    Update flash win names.

--- scripts/zora/flash.tmux.sh
+++ scripts/zora/flash.tmux.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-tmux new-session -d -s flash -n ch
-tmux send-keys "cd $HOME/dev/theflash/ch-connector" "C-m"
+tmux new-session -d -s flash -n storage
+tmux send-keys "cd $HOME/dev/theflash/storage" "C-m"
 
-tmux new-window -n ch-run
+tmux new-window -n storage-run
 tmux send-keys "cd $HOME/dev/theflash/benchmark" "C-m"
 tmux split-window
 tmux send-keys "cd $HOME/dev/theflash/benchmark" "C-m"
 tmux select-pane -t 1
 
 tmux new-window -n spark
-tmux send-keys "cd $HOME/dev/theflash/spark-connector" "C-m"
+tmux send-keys "cd $HOME/dev/theflash/computing" "C-m"
 
 tmux new-window -n spark-debug
 tmux send-keys "cd $HOME/dev/theflash/benchmark/sql-spark" "C-m"

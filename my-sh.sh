c3f4c2b5ebd8077cc2f40b4a316cfcda09ca492e
zanmato1984 <zanmato1984@gmail.com>
2018-09-24 03:13:08 +0800

    Add llvm into path

--- my-sh.sh
+++ my-sh.sh
@@ -8,4 +8,4 @@ elif [ $(uname) = "Linux" ]; then
 fi
 # export SCALA_HOME=/usr/lib/scala-2.11.1
 export GOPATH=$HOME/dev/go
-export PATH=$MY_SH_HOME/scripts/common:$MY_SH_HOME/scripts/$MY_SH_DOMAIN:$JAVA_HOME:$JAVA_HOME/bin:$SCALA_HOME/bin:$MAVEN_HOME/bin:$(go env GOPATH)/bin:$GOROOT/bin:$PATH
+export PATH=$MY_SH_HOME/scripts/common:$MY_SH_HOME/scripts/$MY_SH_DOMAIN:$JAVA_HOME:$JAVA_HOME/bin:$SCALA_HOME/bin:$MAVEN_HOME/bin:$(go env GOPATH)/bin:$GOROOT/bin:/usr/local/opt/llvm/bin:$PATH

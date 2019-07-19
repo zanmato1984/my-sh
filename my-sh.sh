7c6b9a9cff638015a1e173d4cb7aaa2d4aff0a91
zanmato1984 <zanmato1984@gmail.com>
2019-07-20 02:13:26 +0800

    Let /usr/local/bin be ahead of /usr/bin

--- my-sh.sh
+++ my-sh.sh
@@ -7,4 +7,5 @@ fi
 # export SCALA_HOME=/usr/lib/scala-2.11.1
 export GOROOT=/usr/local/opt/go/libexec
 export GOPATH=$HOME/dev/go
-export PATH=$MY_SH_HOME/scripts/common:$MY_SH_HOME/scripts/$MY_SH_DOMAIN:$JAVA_HOME:$JAVA_HOME/bin:$SCALA_HOME/bin:$MAVEN_HOME/bin:$(go env GOPATH)/bin:$GOROOT/bin:/usr/local/opt/llvm/bin:$PATH
+export PATH=$MY_SH_HOME/scripts/common:$MY_SH_HOME/scripts/$MY_SH_DOMAIN:$JAVA_HOME:$JAVA_HOME/bin:$SCALA_HOME/bin:$MAVEN_HOME/bin:$(go env GOPATH)/bin:$GOROOT/bin:/usr/local/opt/llvm/bin:/usr/local/bin:$PATH
+export LC_ALL=en_US.UTF-8

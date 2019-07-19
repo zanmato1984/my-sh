208fbd86382c0cc4f8e9b5c7ac9a666cd6cc9e67
zanmato1984 <zanmato1984@gmail.com>
2019-07-20 01:09:57 +0800

    Update go path

--- my-sh.sh
+++ my-sh.sh
@@ -5,6 +5,6 @@ elif [ $(uname) = "Linux" ]; then
   export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 fi
 # export SCALA_HOME=/usr/lib/scala-2.11.1
-export GOROOT=/usr/local/go
+export GOROOT=/usr/local/opt/go/libexec
 export GOPATH=$HOME/dev/go
 export PATH=$MY_SH_HOME/scripts/common:$MY_SH_HOME/scripts/$MY_SH_DOMAIN:$JAVA_HOME:$JAVA_HOME/bin:$SCALA_HOME/bin:$MAVEN_HOME/bin:$(go env GOPATH)/bin:$GOROOT/bin:/usr/local/opt/llvm/bin:$PATH

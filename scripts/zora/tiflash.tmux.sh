001a39b97f8b30f6c7bf652a29e0d7edbbb78a58
zanmato1984 <zanmato1984@gmail.com>
2019-07-30 18:46:15 +0800

    Reorg tiflash tmux

--- scripts/zora/tiflash.tmux.sh
+++ scripts/zora/tiflash.tmux.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
 
-tmux new-session -d -s tiflash -n storage
-tmux send-keys "cd $HOME/dev/tiflash/storage" "C-m"
-
-tmux new-window -n storage-run
-tmux send-keys "cd $HOME/dev/tiflash/benchmark" "C-m"
-tmux split-window
-tmux send-keys "cd $HOME/dev/tiflash/benchmark" "C-m"
-tmux select-pane -t 1
-
-tmux new-window -n spark
-tmux send-keys "cd $HOME/dev/tiflash/computing" "C-m"
-
-tmux new-window -n spark-run
-tmux send-keys "cd $HOME/dev/tiflash/computing" "C-m"
-
-tmux new-window -n benchmark
-tmux send-keys "cd $HOME/dev/tiflash/benchmark/sql-spark" "C-m"
-tmux split-window
-tmux send-keys "cd $HOME/dev/tiflash/benchmark" "C-m"
-
-tmux new-window -n algernon
+tmux new-session -d -s tiflash -n algernon
 tmux send-keys "cd $HOME/dev/algernon" "C-m"
 tmux split-window
 tmux send-keys "cd $HOME/dev/algernon" "C-m"
@@ -31,8 +11,26 @@ tmux split-window -h
 tmux send-keys "cd $HOME/dev/algernon" "C-m"
 tmux select-pane -t 1
 
-tmux new-window -n tidb
+tmux new-window -n client
 tmux send-keys "cd $HOME/dev/algernon" "C-m"
+tmux split-window -h
+tmux send-keys "cd $HOME/dev/tiflash/storage" "C-m"
+tmux select-pane -t 1
+
+tmux new-window -n ch
+tmux send-keys "cd $HOME/dev/tiflash/storage/ch" "C-m"
+
+tmux new-window -n storage
+tmux send-keys "cd $HOME/dev/tiflash/storage" "C-m"
+
+tmux new-window -n chspark
+tmux send-keys "cd $HOME/dev/tiflash/computing/chspark" "C-m"
+
+tmux new-window -n computing
+tmux send-keys "cd $HOME/dev/tiflash/computing" "C-m"
+
+tmux new-window -n tiflash
+tmux send-keys "cd $HOME/dev/tiflash" "C-m"
 
 tmux select-window -t 1
 tmux a -t tiflash

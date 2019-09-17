59a06600218d720e5d88ab992376dbe8b7582806
zanmato1984 <zanmato1984@gmail.com>
2019-09-17 15:50:01 +0800

    Update tiflash tmux

--- scripts/zora/tiflash.tmux.sh
+++ scripts/zora/tiflash.tmux.sh
@@ -3,7 +3,7 @@
 tmux new-session -d -s tiflash -n algernon
 tmux send-keys "cd $HOME/dev/algernon" "C-m"
 tmux split-window
-tmux send-keys "cd $HOME/dev/algernon" "C-m"
+tmux send-keys "cd $HOME/dev/algernon/log" "C-m"
 tmux split-window -h
 tmux send-keys "cd $HOME/dev/algernon/log" "C-m"
 tmux select-pane -t 1
@@ -19,6 +19,9 @@ tmux select-pane -t 1
 
 tmux new-window -n ch
 tmux send-keys "cd $HOME/dev/tiflash/storage/ch" "C-m"
+tmux split-window
+tmux send-keys "cd $HOME/dev/tiflash/storage/ch/tests" "C-m"
+tmux select-pane -t 1
 
 tmux new-window -n storage
 tmux send-keys "cd $HOME/dev/tiflash/storage" "C-m"
@@ -31,6 +34,9 @@ tmux send-keys "cd $HOME/dev/tiflash/computing" "C-m"
 
 tmux new-window -n tiflash
 tmux send-keys "cd $HOME/dev/tiflash" "C-m"
+tmux split-window
+tmux send-keys "cd $HOME/dev/tiflash/tests" "C-m"
+tmux select-pane -t 1
 
 tmux select-window -t 1
 tmux a -t tiflash

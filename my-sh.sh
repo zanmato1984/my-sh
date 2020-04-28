143f89e0a1dc284c2c3e293934c14ecf34f43736
zanmato1984 <zanmato1984@gmail.com>
2020-04-28 23:39:27 +0800

    Update some path

--- my-sh.sh
+++ my-sh.sh
@@ -1,12 +1,17 @@
+# JAVA home
 if [ $(uname) = "Darwin" ]; then
   export JAVA_HOME=`/usr/libexec/java_home`
 elif [ $(uname) = "Linux" ]; then
   export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 fi
 
+# GO path
 export GOPATH=$HOME/dev/go
 
-export LC_ALL=en_US.UTF-8
+# LLVM
+if [ $(uname) = "Darwin" ]; then
+  export PATH="/usr/local/opt/llvm/bin:$PATH"
+fi
 
 # CUDA
 export PATH=/usr/local/cuda-10.2/bin:/usr/local/cuda-10.2/nsight-compute-2019.5.0${PATH:+:${PATH}}
@@ -17,3 +22,5 @@ export PATH=$JAVA_HOME:$JAVA_HOME/bin:$(go env GOPATH)/bin:$(go env GOROOT)/bin:
 
 # My-SH
 export PATH=$MY_SH_HOME/scripts/common:$MY_SH_HOME/scripts/$MY_SH_DOMAIN:$PATH
+
+export LC_ALL=en_US.UTF-8

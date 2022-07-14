2423701dd9ad742028179683e1615ba8691bcda3
zanmato <zanmato1984@gmail.com>
2022-07-14 17:08:22 +0800

    Update tiflash tmux

--- scripts/zora/tiflash.tmux.sh
+++ scripts/zora/tiflash.tmux.sh
@@ -4,6 +4,7 @@ tmux new-session -d -s tiflash -n algernon
 tmux send-keys "cd $HOME/dev/algernon" "C-m" # Control
 tmux split-window -h
 tmux send-keys "cd $HOME/dev/algernon" "C-m" # Status
+tmux send-keys "./status.sh" "C-m"
 tmux split-window
 tmux send-keys "cd $HOME/dev/algernon/log" "C-m" # Log 1
 tmux split-window
@@ -13,17 +14,8 @@ tmux split-window
 tmux send-keys "cd $HOME/dev/algernon" "C-m" # Client
 tmux select-pane -t 1
 
-tmux new-window -n tics
-tmux send-keys "cd $HOME/dev/tics" "C-m"
-tmux split-window
-tmux send-keys "cd $HOME/dev/tics/tests" "C-m"
-tmux select-pane -t 1
-
 tmux new-window -n tiflash
 tmux send-keys "cd $HOME/dev/tiflash" "C-m"
-tmux split-window
-tmux send-keys "cd $HOME/dev/tiflash/tests" "C-m"
-tmux select-pane -t 1
 
 tmux select-window -t 1
 tmux a -t tiflash

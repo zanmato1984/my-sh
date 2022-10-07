63a29d6b418fa152d4e8b3b9ea3e96d3fb901e1d
zanmato1984 <zanmato1984@gmail.com>
2022-10-07 17:15:44 +0800

    Change proxy

--- oh-my-zsh/common/common.plugin.zsh
+++ oh-my-zsh/common/common.plugin.zsh
@@ -81,8 +81,8 @@ function jpk ()
 
 function proxy ()
 {
-  export http_proxy=http://127.0.0.1:1087
-  export https_proxy=http://127.0.0.1:1087
+  export http_proxy=http://127.0.0.1:7890
+  export https_proxy=http://127.0.0.1:7890
 }
 
 function unproxy ()

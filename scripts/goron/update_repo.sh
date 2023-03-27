cfaa07c0f076d1be7657a2fee595ef1d714f41c5
zanmato <zanmato1984@gmail.com>
2023-03-27 18:23:17 +0800

    Remove deprecated scripts

--- scripts/goron/update_repo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cp -rf $HOME_ABS/.m2/repository/* $HOME_ABS/.ivy2/cache/* /var/www/artifactory/repo
--- scripts/goron/util.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-function find_without_slf4j ()
-{
-  find $* -name "*.jar" | grep -v "[^-]slf4j"
-}

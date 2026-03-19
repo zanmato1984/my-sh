aed55017be56e31aa9be8612b895fee8266a1834
Rossi Sun <zanmato1984@gmail.com>
2026-03-18 23:33:49 -0700

    Remove java

--- my-sh.sh
+++ my-sh.sh
@@ -1,22 +1,8 @@
 export LC_ALL=en_US.UTF-8
 
-# JAVA home
-if [ $(uname) = "Darwin" ]; then
-  export JAVA_HOME=$(/usr/libexec/java_home)
-elif [ $(uname) = "Linux" ]; then
-  export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-fi
-
 # CUDA
 export PATH=/usr/local/cuda-11.1/bin:/usr/local/cuda-11.1/nsight-compute-2020.2.0:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-11.1/lib64:$LD_LIBRARY_PATH
-
-# Languages
-if [ $(uname) = "Darwin" ]; then
-  export PATH=$JAVA_HOME/bin:$PATH
-elif [ $(uname) = "Linux" ]; then
-  export PATH=$JAVA_HOME/bin:$PATH
-fi
 
 # Scratch
 export SCRATCH=/scratch
